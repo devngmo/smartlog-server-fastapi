9f3ffa83c4c416936939904f25ad8cae4e278c40
Tran Manh Linh <devngmo@gmail.com>
2022-12-16 14:46:37 +0700

    CORS: allow all

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import string
-from typing import Union
+from typing import Union, List
 from dotenv import set_key
 from fastapi import Body, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 import json
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=['*', 'http://localhost:8000', 'http://localhost:3000'], allow_methods=['*'], allow_headers=['*'], allow_credentials=True)
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'], allow_credentials=True)
 from memlog import MemoryLogRepository
 
 mlIns = MemoryLogRepository()
-mlIns.addAll('smartlog', ['started'])
+mlIns.addAll('smartlog', [{'type': 'info', 'msg': 'started'}])
 
 @app.get('/')
 def welcome():
     return 'Welcome to SmartLog Server'
 
 @app.post('/log/{appid}')
-async def post_log(appid, request: Request):
-    batch = await request.json()
+async def post_log(appid, batch: List[dict] = Body()):
     print('add %d entries' % len(batch))
-    print(batch[0])
-    print('...')
+    assert isinstance(batch, list)
     mlIns.addAll(appid, batch)
     return 'ok'
 
